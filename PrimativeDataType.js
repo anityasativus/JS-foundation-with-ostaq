6e0f76cda740097c48240b561578623db4337461
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2024-12-12 14:49:32 +0600

    PDT

---
+++ PrimativeDataType.js
@@ -0,0 +1,10 @@
+var a="anitya ";//string
+var b=7;//variable 
+var c=false//boolean
+var e;//undefine
+var f=null;//nothing else 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(e);
+console.log(f);
