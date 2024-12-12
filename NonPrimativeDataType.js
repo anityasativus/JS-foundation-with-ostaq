dcc6f6e07185ac9c9fc27af0105a78e198abe1c6
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2024-12-12 15:00:30 +0600

    Non Primative Data Type

---
+++ NonPrimativeDataType.js
@@ -0,0 +1,13 @@
+
+
+//object 
+var a={ Name : "mew",
+    age:22 ,city:"ctg"
+}
+
+//array
+ var b=['DKA','SHi','ctg '];
+console.log(a.Name);
+console.log(a);
+console.log(b );
+//console.log(a.age);
