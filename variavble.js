14e9a396698901f69dd2cd6e506c7e2a04ba1314
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2024-12-15 19:24:59 +0600

    d

--- variavble.js
+++ variavble.js
@@ -1,6 +1,17 @@
+
 var a=5;
 var b= 6;
 var c= a+b;
 var d=a-b;
 console.log(c);
+console.log(d);
+console.log(d);
+console.log(d);
+var a=5;
+var b= 6;
+var c= a+b;
+var d=a-b;
+console.log(c);
+console.log(d);
+console.log(d);
 console.log(d);
