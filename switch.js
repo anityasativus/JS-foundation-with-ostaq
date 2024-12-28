38bcf87a2f625dffeb6bde38eb9df31e75cf35c9
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2024-12-28 19:15:36 +0600

    XD

---
+++ switch.js
@@ -0,0 +1,20 @@
+var marks=14;
+switch(true)
+{
+    case( marks<=100 && marks>=80 ):
+    console.log('A');
+    break;
+    case( marks<=80 && marks>=70 ):
+    console.log('B');
+    break;
+    case( marks<=70 && marks>=60 ):
+    console.log('C');
+    break;
+    case( marks<=60 && marks>=50 ):
+    console.log('D');
+    break;
+    default: 
+    console.log('fail');
+    
+
+}
